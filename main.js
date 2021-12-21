6ea4c0106f4c3689e41d5aee57cfa7eb3adc2fdc
Lucas Liao <lucasliao6@gmail.com>
2021-12-21 15:22:59 +0100

    Fix double window on mac

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-// main.js
-
-// Modules to control application life and create native browser window
 const { app, BrowserWindow, shell, autoUpdater } = require('electron')
 const path = require('path')
 
@@ -13,7 +10,6 @@ if (process.defaultApp) {
 }
 
 const createWindow = () => {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -25,11 +21,8 @@ const createWindow = () => {
     }
   })
 
-  // and load the index.html of the app.
   mainWindow.loadFile('index.html')
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
 }
 
 const gotTheLock = app.requestSingleInstanceLock()
@@ -38,44 +31,17 @@ if (!gotTheLock) {
   app.quit()
 } else {
   app.on('second-instance', (event, commandLine, workingDirectory) => {
-    // Someone tried to run a second instance, we should focus our window.
     if (mainWindow) {
       if (mainWindow.isMinimized()) mainWindow.restore()
       mainWindow.focus()
     }
   })
 
-  // Create mainWindow, load the rest of the app, etc...
   app.whenReady().then(() => {
     createWindow()
   })
-
-  // Handle the protocol. In this case, we choose to show an Error Box.
-  app.on('open-url', (event, url) => {
-    dialog.showErrorBox('Welcome Back', `You arrived from: ${url}`)
-  })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  if (process.platform === 'darwin') {
-		console.log('toto')
-		createWindow();
-	}
-})
-
-app.on('open-url', (event, url) => {
-  dialog.showErrorBox('Welcome Back', `You arrived from: ${url}`)
-})
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
+  if (process.platform === 'darwin') app.quit()
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
